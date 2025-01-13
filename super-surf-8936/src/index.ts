836dcd6767c7a660efd631ed0392caa985fa68f7
Priyansh Garg <143625974+priyansh-garg1@users.noreply.github.com>
2025-01-13 18:46:39 +0530

    Update index.ts

--- super-surf-8936/src/index.ts
+++ super-surf-8936/src/index.ts
@@ -5,6 +5,14 @@ const app = new Hono<{ Bindings: CloudflareBindings }>()
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+
+app.get('/test2', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/test-post', (c) => {
+  return c.text('Hello Hono!')
+})
 app.get('/test', (c) => {
   return c.json({
     message: 'Hello Hono!',
